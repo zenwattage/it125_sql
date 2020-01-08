8fede028cd03866ac64593b430342b64cb62dfdc
Scott H <zenwattage@gmail.com>
2020-01-08 11:33:26 -0800

    update to joins examples

--- ultMyqlBC/customer_id-Join.sql
+++ ultMyqlBC/customer_id-Join.sql
@@ -20,7 +20,7 @@ CREATE TABLE orders (
 
 INSERT INTO customers ( first_name, last_name, email)
 VALUES ('Boy', 'George', 'george@gmail.com'),
-		('George', 'Michae', 'gm@gmail.com'),
+		('George', 'Michael', 'gm@gmail.com'),
         ('David', 'Bowie', 'david@gmail.com'),
         ('Blue', 'Steele', 'blue@gmail.com'),
         ('Better', 'Davis', 'bette@gmail.com');
@@ -44,13 +44,62 @@ SELECT * FROM orders WHERE customer_id=
     );
     */
     
-    -- CROSSJOIN
-    -- conjoins every customer with every order - basically useless but this is how it's done
-    -- SELECT * FROM customers, orders;
+-- CROSSJOIN
+-- conjoins every customer with every order - basically useless but this is how it's done
+-- SELECT * FROM customers, orders;
     
-     -- IMPLICIT INNER JOIN
-    SELECT * FROM customers, orders WHERE customers.id = orders.customer_id;
+ -- IMPLICIT INNER JOIN
+SELECT * FROM customers, orders WHERE customers.id = orders.customer_id;
        
-    SELECT first_name, last_name, order_date, amount
-    FROM customers, orders
-		WHERE customers.id = orders.customer_id; -- joined where match
+SELECT first_name, last_name, order_date, amount
+FROM customers, orders
+	WHERE customers.id = orders.customer_id; -- joined where match
+        
+ -- EXPLICIT INNER JOIN
+ SELECT * FROM customers_and_orders
+ JOIN orders
+	ON customers.id = orders.customer_id;
+    
+SELECT first_name, last_name, order_date, amount
+FROM customers
+JOIN orders
+	ON customers.id = orders.customer_id;
+    
+    
+-- perform actions on joined data
+-- ORDER BY
+SELECT first_name, last_name, order_date, amount
+FROM customers
+JOIN orders
+	ON customers.id = orders.customer_id
+ORDER BY amount;
+
+ -- identify top spending customers
+SELECT 
+	first_name, 
+    last_name, 
+    order_date, 
+    SUM(amount) AS total_spent
+FROM customers
+JOIN orders
+	ON customers.id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY total_spent DESC;
+
+-- LEFT JOIN
+-- takes everything from customers - and whatever matches from orders
+-- since we get NULL for customers who haven't placed an order
+-- we will replace NULL with 0 using IFNULL
+SELECT 
+	first_name,
+    last_name,
+    IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders
+	ON customers.id = orders.customer_id
+GROUP BY customers.id
+ORDER BY total_spent ASC;
+
+
+-- RIGHT JOIN
+
