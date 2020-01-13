0a131ef73e5fc0907476841f36b672c3695707c3
Scott H <zenwattage@gmail.com>
2020-01-13 13:00:54 -0800

    followers, tags, and photo-tags tables added

--- ultMyqlBC/instagram_clone.sql
+++ ultMyqlBC/instagram_clone.sql
@@ -33,19 +33,33 @@ CREATE TABLE likes (
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photo(id),
-    PRIMARY KEY(user_id, photo_id) -- prevent repeat of likes
+    PRIMARY KEY(user_id, photo_id) -- prevent duplication of likes
 );
 
--- CREATE TABLE hashtags (
--- 	id
---     tags
--- );
 
--- CREATE TABLE followers/following (
--- 	id
---     follow_username
--- );
+CREATE TABLE followers (
+	follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    create_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (followee_id) REFERENCES users(id),
+    PRIMARY KEY (follower_id, followee_id)
+);
 
+CREATE TABLE tags (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+	photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+    
+);
 
 INSERT INTO users (username) VALUES
 ('ScottyPotty'),
@@ -53,4 +67,4 @@ INSERT INTO users (username) VALUES
 
 SELECT * FROM users;
 
-DESCRIBE comments;
+DESCRIBE likes;
