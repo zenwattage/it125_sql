bf612712cc669fe19b59bdeb2ae8ac218d978ea3
Scott H <zenwattage@gmail.com>
2020-01-13 09:40:28 -0800

    adding users, comments, likes tables

--- ultMyqlBC/instagram_clone.sql
+++ ultMyqlBC/instagram_clone.sql
@@ -1,12 +1,55 @@
-/*
-instagram clone
 
-users
-photos
-comments
-likes
-hashtags
-followers/following
+DROP DATABASE IF EXISTS ig_clone;
+CREATE DATABASE ig_clone;
+USE ig_clone;
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at	TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)    
+);
+
+CREATE TABLE comments (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+        comment_text VARCHAR(255) NOT NULL,
+        user_id	INT NOT NULL,
+        photo_id INT NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW(),
+        FOREIGN KEY(user_id) REFERENCES users(id),
+        FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+CREATE TABLE likes (
+	user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photo(id)
+);
+
+-- CREATE TABLE hashtags (
+-- 	id
+--     tags
+-- );
+
+-- CREATE TABLE followers/following (
+-- 	id
+--     follow_username
+-- );
 
 
-*/
+INSERT INTO users (username) VALUES
+('ScottyPotty'),
+('CharlieBrown');
+
+SELECT * FROM users;
+
+DESCRIBE comments;
