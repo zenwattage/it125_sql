399ede377be84c057838e7c4f83dff0919dcc8db
zenwattage <zenwattage@gmail.com>
2020-02-17 08:29:43 -0800

    update to collegeDb

---
+++ ultMyqlBC/testingCollegeDb.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS collegeDb;
+CREATE DATABASE collegeDb;
+USE collegeDb;
+
+
+CREATE TABLE quarter (
+	quarterId INT Primary Key AUTO_INCREMENT,
+    name VARCHAR(45),
+    year SMALLINT
+);
+INSERT INTO quarter
+VALUES (DEFAULT, 'Fall', '2019');
+
+CREATE TABLE section (
+	sectionId INT PRIMARY KEY AUTO_INCREMENT,
+    name	ENUM('Day','Night', 'Online')    
+);
+
+-- designation should be our primary key?
+CREATE TABLE course (
+	courseId INT PRIMARY KEY AUTO_INCREMENT,
+    designation VARCHAR(7),
+    name	VARCHAR(45),
+    credits	TINYINT
+);
+INSERT INTO course
+VALUES (DEFAULT, 'MATH101', 'GEOMETRY', 5);
+
+
+
+
+INSERT INTO section
+(
+
+SELECT * FROM course;
+SELECT * FROM section;
+SELECT * FROM quarter;
+
