614727a728565ba4af17a487e166907d0909075a
Scott H <zenwattage@gmail.com>
2020-02-22 08:41:01 -0800

    aquariumDb start

--- als_aquariums.sql
+++ als_aquariums.sql
@@ -4,3 +4,31 @@ CREATE DATABASE als_aquariums;
 
 USE als_aquariums;
 
+CREATE TABLE fish (
+	FishId INT PRIMARY KEY AUTO_INCREMENT,
+    FishName VARCHAR(20) NOT NULL,
+    FishType ENUM('Salt', 'Fresh') NOT NULL
+);
+
+CREATE TABLE Customer (
+	CustomerId INT PRIMARY KEY AUTO_INCREMENT,
+    CustomerLName VARCHAR(15) NOT NULL,
+    CustomerFName VARCHAR(15) NOT NULL,
+    CustPhone CHAR(10)
+);
+
+CREATE TABLE Aquarium (
+	AquariumId INT PRIMARY KEY AUTO_INCREMENT,
+    AquariumGallons SMALLINT(3) NOT NULL,
+    AquariumFinish ENUM('Wood','Black') NOT NULL
+);
+
+CREATE TABLE Pump (
+	PumpId INT PRIMARY KEY AUTO_INCREMENT,
+    PumpType ENUM('Salt','Fresh') NOT NULL,
+    PumpFlowRateGPM SMALLINT(4) NOT NULL
+);
+
+CREATE TABLE Cust_Order (
+	OrderNum INT PRIMARY KEY AUTO_INCREMENT
+);
